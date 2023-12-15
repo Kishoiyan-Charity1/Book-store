01d7186d6bf8bc3b59b828d73ed31017acaf2fb9
Kishoiyan-Charity1 <charity.kishoiyancharity@gmail.com>
2023-12-15 20:47:48 +0300

    changes

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,15 @@ app.use(express.json());
 //Middleware for handling CORS POLICY
 
 //OPTION 1: Allow all origins with default of cors
-// app.use(cors());
+app.use(cors());
 //option 2: allow custom origins
-app.use(
-    cors({
-        origin:'http://localhost:3000',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type'],
-    })
-);
+// app.use(
+//     cors({
+//         origin:'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
 
 
 
